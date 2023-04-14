e66c70851ef0dae8c58e5daccffbcd31acfc9b79
番茄炒西红柿 <healeryan@163.com>
2023-04-14 17:00:43 +0800

    排序算法 -> 归并排序、基数排序（桶排序）

---
+++ DataStructure/src/com/study/sortAlgorithm/MergeSort.java
@@ -0,0 +1,68 @@
+package com.study.sortAlgorithm;
+
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * 归并排序
+ * 采用经典的分治理念
+ * @author yan
+ */
+public class MergeSort {
+    static int arrSize = 80000000;
+    public static void main(String[] args){
+//        int arr[] = {8,5,3,2,1,6,4,7};
+        int arr[] = new int[arrSize];
+        CommonUtil.paddingArr(arr);
+        int[] temp = new int[arr.length];
+        System.out.println(CommonUtil.sdf.format(new Date()));
+        mergeSort(arr,0,arr.length-1,temp);
+        System.out.println(CommonUtil.sdf.format(new Date()));
+//        System.out.println(Arrays.toString(arr));
+    }
+
+
+    private static void mergeSort(int[] arr,int left,int right,int[] temp){
+        if (left < right){
+            int mid = (left + right) / 2;
+            // 通过递归拆分 左边的数据
+            mergeSort(arr,left,mid,temp);
+            // 通过递归拆分 右边的数据
+            mergeSort(arr,mid + 1,right,temp);
+            // 合并
+            merge(arr,left,mid,right,temp);
+        }
+    }
+
+    /**
+     * 合并已经分好了的数据
+     * @param arr 需要排序的原始数组
+     * @param left 左侧下标
+     * @param mid 中间下标
+     * @param right 右边下标
+     * @param temp 辅助数组 用来进行排序合并
+     */
+    private static void merge(int[] arr, int left, int mid, int right, int[] temp){
+        int i = left;
+        int j = mid + 1;
+        int t = 0;
+        // 1、合并排序 通过left与right 合并排序添加到temp中
+        while (i <= mid && j <= right){
+            if (arr[i] <= arr[j])
+                temp[t] = arr[i++];
+             else
+                temp[t] = arr[j++];
+            t++;
+        }
+        // 2、将left 或者 right 没有添加到temp的元素全部添加到temp中
+        while (i <= mid)
+            temp[t++] = arr[i++];
+        while (j <= right)
+            temp[t++] = arr[j++];
+        // 3、将temp中的元素copy到arr中
+        int l = left;
+        t = 0;
+        while (l <= right)
+            arr[l++]= temp[t++];
+    }
+}
---
+++ DataStructure/src/com/study/sortAlgorithm/RadixSort.java
@@ -0,0 +1,67 @@
+package com.study.sortAlgorithm;
+
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * 基数排序（桶排序）
+ * @author yan
+ */
+public class RadixSort {
+    static int arrSize = 800000;
+    public static void main(String[] args) {
+//        int[] arr = {123,34,65,76,324,9,123,1,4};
+        int[] arr = new int[arrSize];
+        CommonUtil.paddingArr(arr);
+        System.out.println(CommonUtil.sdf.format(new Date()));
+        radixSort(arr);
+        System.out.println(CommonUtil.sdf.format(new Date()));
+//        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 基数排序是一个经典的已空间换时间的排序算法
+     * 注意：
+     *  基数排序中如果含有小数或者负数需要特殊处理  下面的方式不建议使用负数或小数
+     *  也可以使用其他的排序算法
+     *  使用基数排序时如果数据量过大会出现 OutOfMemoryError 内存不够的异常  谨慎使用！！！
+     *  80000000 个int类型的数据 大概需要如下内存空间  80000000*11*4/1024/1024/1024 = 3.278255462646484 （GB）
+     * @param arr 需要排序的原始数组
+     */
+    private static void radixSort(int[] arr){
+        // 1、定义桶 （二维数组）
+        int[][] bucket = new int[10][arr.length];
+        // 定义记录每个桶中 一维数组的下标值
+        int[] bucketElement = new int[10];
+
+        int maxVal = arr[0];
+        // 获取数组中的最大值 并求出最大数值的长度
+        for (int i = 1; i < arr.length; i++) {
+            if (maxVal < arr[i]) maxVal = arr[i];
+        }
+        int maxLength = (""+maxVal).length();
+
+        // 下面进行基数排序  基数排序需要 循环maxLength 次
+        for (int i = 0,n = 1; i < maxLength; i++,n*=10) {
+            // 循环arr 将值填充到桶排序中  第一次取个位数、第二次取十位数、第三次取百位数进行比较 以此类推...
+            for (int j = 0; j < arr.length; j++) {
+                // 依次获取 个十百千 位数
+                int bucketIndex = arr[j] / n % 10;
+                // 得到bucketIndex 就是桶的下标 然后将该数填充到对应下标的桶中
+                bucket[bucketIndex][bucketElement[bucketIndex]++] = arr[j];
+            }
+            // 按从小到大的顺序取出桶中的数据
+            int arrIndex = 0;
+            for (int j = 0; j < bucket.length; j++) {
+                if (bucketElement[j] > -1){
+                    // 取出当前桶中的所有数据
+                    for (int k = 0; k < bucketElement[j]; k++) {
+                        arr[arrIndex++] = bucket[j][k];
+                    }
+                }
+                // 清空该桶中的下标索引值
+                bucketElement[j] = 0;
+            }
+        }
+    }
+}
